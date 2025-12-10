18bf506c657a23287021af7efdedef9f72816c47
ashimbabu <ashimbabu@gmail.com>
2025-12-10 07:22:43 +0545

    My First Commit

---
+++ List.py
@@ -0,0 +1,21 @@
+# List (Data Structure)
+# name = ["Ram", "Shyam", "Hari", "Mohan", "Amish", "Chandra"]
+# name.append("Sohan") # Adding value at the end of list
+# name.insert(1,"Sohan") # Adding value at specific index
+# name.pop() # Removing last from list
+# name.remove("Hari") # Removing specific value from list
+# print(name)
+
+# x = name.index("Shyam") # Getting index of specific data
+# print(x)
+
+# name.sort() # Sorting the list in ascending order
+# name.reverse() # Reversing the list
+# name.extend("Sita", "Gita", "Rita", "Radha", "Alka") # Adding multiple values at the end of list
+# print(name)
+
+ruppe = [1000, 5000, 2000, 4000, 3000]
+# ruppe.sort() # Ascending order
+# # ruppe.sort(reverse=True) # Decending order
+# ruppe.reverse() # Reversing the list
+print(ruppe)
