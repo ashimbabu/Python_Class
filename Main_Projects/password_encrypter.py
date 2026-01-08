450b0e0f057b802b8b2c4a04c78bb2e263502b08
ashimbabu <ashimbabu@gmail.com>
2026-01-08 20:56:06 +0545

    Password Encrypter

--- Main_Projects/password_encrypter.py
+++ Main_Projects/password_encrypter.py
@@ -11,21 +11,21 @@ import os
 import hashlib   # this library is used to encrypt the password in hashkey
 from tkinter  import *     # for gui (Graphical User Interface)
 from tkinter import messagebox
-from openpyxl import Workbook , load_workbook      # for manipulating the excel file
+from openpyxl import Workbook , load_workbook  # for manipulating the excel file
 
 file_path = "C:/Users/acer/Desktop/Python/Main_Projects/User.xlsx"
 
 def encrypt_password(password):   # to encrypt the user given password
     encrypted_password =  hashlib.sha256(password.encode()).hexdigest()
     return encrypted_password
-def create_file():                                # it checks whether given file exists or not, if not exist create the given excel file
+def create_file():     # it checks whether given file exists or not, if not exist create the given excel file
     if not os.path.exists(file_path):
         wb = Workbook()
         sheet = wb.active
         sheet.title = "Users"                     # it puts the title Users.xlsx
         sheet.append(["Username","Password"])     # it puts the "Username" and "Password" in the first row of excel sheet
         wb.save(file_path)
-def register_user():         # it takes user given input from GUI, checks if any fields are empty, encrypt the given password, checks if username already exists or not, then stores the user given "username" and "encrypted password" in excel file
+def register_user():    # it takes user given input from GUI, checks if any fields are empty, encrypt the given password, checks if username already exists or not, then stores the user given "username" and "encrypted password" in excel file
     
     username = entry_username.get()
     password = entry_password.get()
@@ -47,7 +47,7 @@ def register_user():         # it takes user given input from GUI, checks if any
     messagebox.showinfo("Success","User registered sucessfully")
     clear_fields()
     
-def login_user():           #it takes user given input from GUI, encrypt the given password, checks if user given "username" and "password" match with any data in the excel sheets, if matched show login message,
+def login_user():      # it takes user given input from GUI, encrypt the given password, checks if user given "username" and "password" match with any data in the excel sheets, if matched show login message,
     
     username = entry_username.get()
     password = entry_password.get()
@@ -65,7 +65,7 @@ def login_user():           #it takes user given input from GUI, encrypt the giv
     messagebox.showerror("Error","Login failed") 
     clear_fields() 
             
-def clear_fields():               # clears the fields of username and password when "register" or "login" button is clicked
+def clear_fields():     # clears the fields of username and password when "register" or "login" button is clicked
     entry_username.delete(0,END)
     entry_password.delete(0,END)
     
